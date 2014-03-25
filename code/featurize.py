c09530ceac0b1f7d5da981d03bb671e84045a06c
Tianwei Li <lee.tianwei@gmail.com>
2014-03-24 21:19:53 -0400

    add feature extraction for word count

--- code/featurize.py
+++ code/featurize.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
 import sys
+from sets import Set
+
+def filtStopWords(wordsOrg, stopWordSet):
+    words = []
+    for word in wordsOrg:
+        if word not in stopWordSet:
+            words.append(word)
+    
+    return words
+
 
 def featurize(train_articles, test_articles):
     '''
@@ -24,32 +34,71 @@ def featurize(train_articles, test_articles):
     and inspecting the return value.
     '''
 
-    ### REPLACE THE REST OF THIS FUNCTION WITH YOUR FEATURE GENERATION CODE ###
-    def make_features(articles):
-        return [[len(article.split()), int(article.startswith('the'))] for article in articles]
-    return (make_features(train_articles), make_features(test_articles))
-
-def articles_to_features(train_in_name, train_out_name, test_in_name, test_out_name):
+    # generate the stop words hash set
     stopword_file = open("stopwords.txt", 'r')
-    #for line in stopword_file:
     content = stopword_file.read().strip()
     stop_words = content.split('\n')
-    print('tttt')
+    stopWordSet = Set(stop_words)
+
+    # create dictionary
+    wordDic = {}
+    wordMatrix = []
+    for article in train_articles:
+        words = article.strip().split()
+        words = filtStopWords(words, stopWordSet)
+        wordMatrix.append(words)
+        for word in words:
+            if word not in wordDic:
+                wordDic[word] = 0
+
+
+    # generate word count for train articles
+    trainFeatures = []
+    for words in wordMatrix:
+        for word in words:
+            wordDic[word] += 1
+        trainFeatures.append(wordDic.values())
+        # clear the dictionary
+        for k in wordDic.keys() : wordDic[k] = 0
     
-#    with open(train_in_name, 'r') as in_file:
-#        train_lines = in_file.readlines()
-#    with open(test_in_name, 'r') as in_file:
-#        test_lines = in_file.readlines()
-#
-#    train_features, test_features = featurize(train_lines, test_lines)
-#
-#    for features, out_name in ((train_features, train_out_name),
-#                               (test_features, test_out_name)):
-#        with open(out_name, 'wb') as csvfile:
-#            # Output features in MATLAB-readable CSV format
-#            for row in features:
-#                csvfile.write(', '.join([str(feature) for feature in row]))
-#                csvfile.write('\n')
+    
+    # generate word count for test articles
+    testFeatures = []
+    for article in test_articles:
+        words = article.strip().split()
+        words = filtStopWords(words, stopWordSet)
+        for word in words:
+            if word in wordDic: wordDic[word] += 1
+        testFeatures.append(wordDic.values())
+        # clear the dictionary
+        for k in wordDic.keys() : wordDic[k] = 0
+        
+    return (trainFeatures, testFeatures)
+
+
+
+
+    ### REPLACE THE REST OF THIS FUNCTION WITH YOUR FEATURE GENERATION CODE ###
+#    def make_features(articles):
+#        return [[len(article.split()), int(article.startswith('the'))] for article in articles]
+#    return (make_features(train_articles), make_features(test_articles))
+
+def articles_to_features(train_in_name, train_out_name, test_in_name, test_out_name):
+    
+    with open(train_in_name, 'r') as in_file:
+        train_lines = in_file.readlines()
+    with open(test_in_name, 'r') as in_file:
+        test_lines = in_file.readlines()
+
+    train_features, test_features = featurize(train_lines, test_lines)
+
+    for features, out_name in ((train_features, train_out_name),
+                               (test_features, test_out_name)):
+        with open(out_name, 'wb') as csvfile:
+            # Output features in MATLAB-readable CSV format
+            for row in features:
+                csvfile.write(', '.join([str(feature) for feature in row]))
+                csvfile.write('\n')
 
 if __name__ == '__main__':
     articles_to_features(*sys.argv[1:5])
